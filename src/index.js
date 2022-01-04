419d1e37b95d697a26c4586a7a7b167533a9389f
devEl <klimelm@gmail.com>
2022-01-04 15:28:38 +0300

    init: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const newLocal = +n.toString()
+                        .split('')
+                        .filter(e=> !isNaN(+e))
+                        .reverse()
+                        .join('');
+  return newLocal;
 }
